b7f74dea020e5339861632f93a390b08dedefab1
Rajiv Iyer <raju.rgi@gmail.com>
2023-09-12 18:58:40 +0530

    update new version 0.1.2 in setup files

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/rajiviyer/genai_evaluation',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
